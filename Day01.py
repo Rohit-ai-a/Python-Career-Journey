234d6cea8d876c9f423fad98053e6e58a7b3c281
Rohit <rohit10rg54@gmail.com>
2026-06-20 19:43:22 +0530

    Day01 of python, We learn today how to, print the statements also typecasting , storing mutli-word variable also printing multiwords. Types of Cases and variables

---
+++ Day01.py
@@ -0,0 +1,31 @@
+# First day of learning python from basics
+print("Hello World")
+
+# python is an language with the help of
+# we can build applications websites and all
+
+# This is how we can create a variable with inseerting 10 initial value
+n = 10
+x = 'Rohit'
+
+# tyepcasting
+n = str(10)
+
+# Multi-Words variable cases:
+
+# Camel case: first letter is small then every words first lett will be Cap
+myCarIsParked = False
+
+# PascalCase: Every words first letter will be capital
+MyCarIsParked = True
+
+# snake case: Every word should be divided by '_' and every letters are in Lower case
+my_car_is_parked = False
+
+print(myCarIsParked)
+print(myCarIsParked)
+print(my_car_is_parked)
+
+# How to asssign Multiple values in python
+fruit, vegetable, biscuit = 'apple','Capsicum','Parle'
+print(fruit, vegetable, biscuit, end=" here we have one fruit one veges and one biscuit")
